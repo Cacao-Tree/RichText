47cc072bf987d035da41f190be56c78258dc285d
wangyang25 <wangyang25@300624.cn>
2025-07-05 17:03:59 +0800

    feat: 分支切换以及清除

--- src/static/vue-js/refV2.js
+++ src/static/vue-js/refV2.js
@@ -1,3 +1,7 @@
+// 首先调整effect函数的实现，增加一个effectFn来收集activeEffect，将这个effectFn赋值给activeEffect，是因为需要增加一个deps数组，来收集与这个副作用函数相关的依赖，和一个清除函数来清除与该副作用函数相关的依赖集合。
+// 收集依赖的函数track，增加了一个流程，将当前key的集合push到activeEffect.deps中，这样就能知道这个副作用函数和哪些依赖相关联了，然后在清除中就可以把集合中和这个副作用函数相关的依赖清除掉。
+
+
 const bucket = new WeakMap();
 
 const data = {
@@ -10,7 +14,7 @@ let activeEffect;
 function effect(fn) {
   const effectFn = () => {
     cleanup(effectFn);
-    activeEffect = fn;
+    activeEffect = effectFn;
     fn();
   };
 
@@ -19,7 +23,12 @@ function effect(fn) {
   effectFn();
 }
 
-function cleanup(effectFn) {}
+function cleanup(effectFn) {
+  for (let item of effectFn.deps) {
+    item.delete(effectFn);
+  }
+  effectFn.dep.length = 0;
+}
 
 function track(target, key) {
   let depsMap = bucket.get(target); // 这个代理对象的副作用依赖
@@ -37,6 +46,14 @@ function track(target, key) {
   activeEffect.deps.push(dep); // 将与当前副作用函数相关的依赖集合收集起来
 }
 
+function trigger(target, key) {
+  const depsMap = bucket.get(target);
+  const dep = depsMap?.get(key);
+
+  const effectsToRun = new Set(dep); // 新增
+  effectsToRun.forEach((effectFn) => effectFn()); // 新增
+}
+
 const obj = new Proxy(data, {
   get: (target, key) => {
     if (!activeEffect) return target[key];
@@ -48,16 +65,11 @@ const obj = new Proxy(data, {
   set: (target, key, newVal) => {
     target[key] = newVal;
 
-    const depsMap = bucket.get(target);
-    const dep = depsMap?.get(key);
-
-    if (dep) {
-      dep.forEach((fn) => fn());
-    }
+    trigger(target, key);
   },
 });
 
-effect(() => {
+effect(function effectFn() {
   console.log('执行');
   document.getElementById('content').innerText = obj.confirm ? obj.num : 'not';
 });
